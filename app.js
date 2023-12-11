7ffe7a599edeaaf8b487112d8e349f46cc30358a
Hritik-Jaiswal <hritikj2208@gmail.com>
2023-12-11 16:02:53 +0530

    skeleton ready

---
+++ app.js
@@ -0,0 +1,25 @@
+// Importing needed libraries
+const discord = require('discord.js')
+const axios = require('axios')
+const dotenv = require('dotenv')
+
+const client = new discord.Client()
+const token = process.env.DISCORD_TOKEN 
+const targetUser = process.env.
+
+// Connect it with discord
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.on('ready', ()=> {
+    // ignore its own messages
+    if (message.author.id) return
+
+    // targeted user
+    if(message.author.id === targetUser) {
+        // TODO: react for the user
+    }
+})
+
+client.token(token)
